a16e56db8cd3ed21d34128e82e29a8553511882e
Firmansyah <fafirmansyah20@gmail.com>
2018-08-28 10:25:30 +0700

    ahlikasi v1.0

---
+++ app/src/main/java/com/ahlikasir/aplikasi/kasironline/Activity/laporan/LaporanPendapatanActivity.kt
@@ -0,0 +1,13 @@
+package com.ahlikasir.aplikasi.kasironline.Activity.laporan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ahlikasir.aplikasi.kasironline.R
+
+class LaporanPendapatanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laporan_pendapatan)
+    }
+}
